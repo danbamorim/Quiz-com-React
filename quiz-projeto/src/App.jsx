f48848c5813c35b064df395581ead8689ef4c09d
Danilo Amorim <daniloamorim@MacBook-Pro-de-Danilo.local>
2024-06-25 20:25:41 -0300

    first commit

--- quiz-projeto/src/App.jsx
+++ quiz-projeto/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import { Welcome } from './components/Welcome'
 
 function App() {
-  
+
   return (
     <div className='app'>
       <h1>QUIZ</h1>
-
+      <Welcome />
     </div>
   )
 }
---
+++ quiz-projeto/src/components/Welcome.jsx
@@ -0,0 +1,12 @@
+
+export const Welcome = () => {
+    return (
+        <div>
+            <H2>seja bem vindo</H2>
+            <p>cliqie no botao abaixo pra comecar</p>
+            <button>Iniciar</button>
+        </div>
+    )
+}
+
+export default Welcome;
