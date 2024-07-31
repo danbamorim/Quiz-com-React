b8155923460f1361d1a4cb9d5642d325ae826626
Danilo Amorim <daniloamorim@MacBook-Pro-de-Danilo.local>
2024-07-31 20:14:02 -0300

    adicionando botao

--- quiz-projeto/src/components/Welcome.jsx
+++ quiz-projeto/src/components/Welcome.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react"
 import Quiz from "../img/quiz.svg"
 import "./Welcome.css"
+import { QuizContext } from "../context/quiz"
 const Welcome = () => {
+    const [quizState, dispatch] = useContext(QuizContext)
     return (
         <div id="welcome">
             <h2>Seja bem-vindo</h2>
